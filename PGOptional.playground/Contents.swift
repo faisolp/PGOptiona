6e7f55258f5c0cb77c0f53041758b6e1a20a4fdd
faisolp <faisolp@gmail.com>
2017-09-10 09:25:53 +0700

    Room

--- PGOptional.playground/Contents.swift
+++ PGOptional.playground/Contents.swift
@@ -4,9 +4,19 @@ import UIKit
 
 //Optional Problem
 var strNumber = "Five"
-var intNumber = Int(strNumber)
-intNumber = (intNumber != nil ? intNumber :0)
-var intAnswerA: Int = intNumber! + 10
+var intAnswerA:Int = 0
+//Fix Error  not Number
+var intNumber = Int(strNumber)  //1
+
+if var intNumber2 = intNumber {
+    
+    intAnswerA = intNumber2 + 10
+}
+print("intAnswerA=>\(intAnswerA)")
+//2
+//intNumber = (intNumber != nil ? intNumber :0)
+
+//    intAnswerA: Int = intNumber! + 10
 
 
 
