be11c877d136dae50cfac84823322758a4236040
faisolp <faisolp@gmail.com>
2017-09-10 08:22:25 +0700

    test123

---
+++ PGOptional.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Optional Problem
+var strNumber = "Five"
+var intNumber = Int(strNumber)
+intNumber = (intNumber != nil ? intNumber :0)
+var intAnswerA: Int = intNumber! + 10
+
+
+
+
